4f1ae05ff62b46f53dd02ca02e2c39b96bcce764
Pro-Sammo <ss.sammo2002@gmail.com>
2024-01-12 16:03:36 +0600

    change-cros-origin-fix

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ const app = express();
 dotenv.config();
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
